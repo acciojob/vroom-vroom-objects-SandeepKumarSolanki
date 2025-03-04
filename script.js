452db7ed89d145ac5474f4a6cf6689d3322be186
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-04 13:44:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function Car(make, model) {
 
 // Car Prototype Method
 Car.prototype.getMakeModel = function() {
-    return `${this.make} ${this.method}`;
+    return `${this.make} ${this.model}`;
 };
 
 function SportsCar(make, model, topSpeed) {
